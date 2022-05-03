d4a9cf20d2d4e9784de8bab46ee22a3e59ba3ece
Yunus AK <yunus.ak.gen.tr@gmail.com>
2022-05-03 22:04:16 +0000

    sonuç listeleme

--- index.php
+++ index.php
@@ -113,7 +113,7 @@ $dersler = [
 
 function getFormValue($var = null)
 {
-    $var = array_key_exists($var,$_POST) ? intval($_POST[$var]) : 0;
+    $var = array_key_exists($var, $_POST) ? intval($_POST[$var]) : 0;
     return $var;
 }
 function getFormValues()
@@ -136,13 +136,13 @@ function getFormValues()
 
 function isFormSubmitted()
 {
-    return isset($_POST['submit_hesapla']);
+    return isset($_POST['hsp_hesapla']);
 }
 
 try {
     if (isFormSubmitted()) {
         $errors = getFormValues();
-    if ($errors != '') throw new Exception($errors);
+        if ($errors != '') throw new Exception($errors);
     } else {
         $sonuc = 'Değerleri girin ve hesapla butonuna basın';
     }
@@ -154,78 +154,75 @@ try {
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-* {
-  box-sizing: border-box;
-}
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        * {
+            box-sizing: border-box;
+        }
 
-/* Create three equal columns that floats next to each other */
-.column {
-  float: left;
-  width: 31%;
-  padding: 10px;
-  margin: 1%;
-  
-}
+        /* Create three equal columns that floats next to each other */
+        .column {
+            float: left;
+            width: 31%;
+            padding: 10px;
+            margin: 1%;
 
-/* Clear floats after the columns */
-.row:after {
-  content: "";
-  display: table;
-  clear: both;
-}
-</style>
+        }
+
+        /* Clear floats after the columns */
+        .row:after {
+            content: "";
+            display: table;
+            clear: both;
+        }
+    </style>
 </head>
+
 <body>
-<form method="post" action="#" name="lgs_form" id="lgs_form">
-<div class="row" >
-  <div class="column" style="color:#FAFAFA;background-color:#8A604F">Ders</div>
-  <div class="column" style="color:#FAFAFA;background-color:#8A604F">Doğru</div>
-  <div class="column" style="color:#FAFAFA;background-color:#8A604F">Yanlış</div>
-</div>
-<?php foreach ($dersler as $ders) : ?>
-    <div class="row" >
-                    <div class="column">
-                        <b><?= $ders->adi ?> (<?= $ders->soruSayisi ?>)</b>
-                    </div>
-                    <div class="column">
-                        <input 
-                            name="<?= $ders->formNameDogru ?>" 
-                            type="number" 
-                            class="form-control" 
-                            value="<?= $ders->dogruSayisi ?>" 
-                            max="<?= $ders->soruSayisi ?>" 
-                            min="0">
-                    </div>
-                    <div class="column">
-                        <input 
-                            name="<?= $ders->formNameYanlis ?>" 
-                            type="number" 
-                            class="form-control" 
-                            value="<?= $ders->yanlisSayisi ?>" 
-                            max="<?= $ders->soruSayisi ?>" 
-                            min="0">
-                    </div>
-    </div>
-<?php endforeach; ?>
+    <form method="post" action="#" name="lgs_form" id="lgs_form">
+        <div class="row">
+            <div class="column" style="color:#FAFAFA;background-color:#8A604F">Ders</div>
+            <div class="column" style="color:#FAFAFA;background-color:#8A604F">Doğru</div>
+            <div class="column" style="color:#FAFAFA;background-color:#8A604F">Yanlış</div>
+        </div>
+        <?php foreach ($dersler as $ders) : ?>
+            <div class="row">
+                <div class="column">
+                    <b><?= $ders->adi ?> (<?= $ders->soruSayisi ?>)</b>
+                </div>
+                <div class="column">
+                    <input name="<?= $ders->formNameDogru ?>" type="number" class="form-control" value="<?= $ders->dogruSayisi ?>" max="<?= $ders->soruSayisi ?>" min="0">
+                </div>
+                <div class="column">
+                    <input name="<?= $ders->formNameYanlis ?>" type="number" class="form-control" value="<?= $ders->yanlisSayisi ?>" max="<?= $ders->soruSayisi ?>" min="0">
+                </div>
+            </div>
+        <?php endforeach; ?>
 
 
-  
-    <div class="row">
-  <div class="column"></div>
-  <div class="column"> <input aria-label="hesapla" type="submit" name="hsp_hesapla" value="LGS PUANIMI HESAPLA" class="btn btn-success mr-2 text-center" style="width:99%;color:#FFFFFF;background-color:#1E623F"></div>
-  <div class="column"><input aria-label="temizle" type="button" name="reset hsp_temizle" value="Temizle" class="btn btn-info" onclick="if (!window.__cfRLUnblockHandlers) return false; teogTemizle()" style="width:99%; background-color:#7A0000; float:right;color:#FFFFFF"></div>
-</div>
-    <div style="margin-bottom:15px;">
-       
-        
-    </div>
-</form>
-<?php if ($isError) { ?>
-    <h3>Hata</h3>
-    <p><?= $errorMessage ?></p>
-<?php } else if ($sonuc != '') { ?>
-    <h3>Sonuçlar</h3>
-<?php } ?>
+
+        <div class="row">
+            <div class="column"></div>
+            <div class="column"> <input aria-label="hesapla" type="submit" name="hsp_hesapla" value="LGS PUANIMI HESAPLA" class="btn btn-success mr-2 text-center" style="width:99%;color:#FFFFFF;background-color:#1E623F"></div>
+            <div class="column"><input aria-label="temizle" type="button" name="reset hsp_temizle" value="Temizle" class="btn btn-info" onclick="if (!window.__cfRLUnblockHandlers) return false; teogTemizle()" style="width:99%; background-color:#7A0000; float:right;color:#FFFFFF"></div>
+        </div>
+        <div style="margin-bottom:15px;">
+
+
+        </div>
+    </form>
+    <?php if ($isError) { ?>
+        <h3>Hata</h3>
+        <p><?= $errorMessage ?></p>
+    <?php } else if (!isFormSubmitted()) { ?>
+        Soru sayilarini girin...
+    <?php } else { ?>
+        <h3>Sonuçlar</h3>
+        <?php foreach ($dersler as $ders) : ?>
+            <p>
+                <b><?= $ders->adi ?> </b> Ham Puan : <?= $ders->hamPuan() ?>
+            </p>
+        <?php endforeach; ?>
+    <?php } ?>
